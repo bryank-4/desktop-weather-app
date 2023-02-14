59ff2377260ccf53a62b9dc60ad5a6774e4124b1
joshuaobare <jobare.obare@gmail.com>
2023-02-14 17:42:04 +0300

    added update method to DBConnection class to insert current weather data into the DB

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,20 +1,22 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSet;
+import com.google.gson.internal.LinkedTreeMap;
+import org.json.simple.JSONObject;
+
+import java.sql.*;
+import java.util.Map;
 
 public class DBConnection {
-    Connection conn = null;
+    private Connection conn = null;
+    private Statement statement = null;
+    private ResultSet rs = null;
 
     public DBConnection() {
         connect();
-        select();
     }
 
     public void connect() {
+        // the connection to the database is made. an exception is thrown is the connection fails
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/users?user=root");
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/desktopweatherapp?user=root");
             System.out.println(conn);
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
@@ -24,8 +26,6 @@ public class DBConnection {
     }
 
     public void select() {
-        Statement statement = null;
-        ResultSet rs = null;
 
         try {
             statement = conn.createStatement();
@@ -39,6 +39,25 @@ public class DBConnection {
         }
     }
 
+    public void update(LinkedTreeMap obj, Map locationData) {
+
+        try {
+            PreparedStatement prstatement = conn.prepareStatement("INSERT INTO  CURRENT_WEATHER (userID , location , " +
+                    "weathercode , temperature , windspeed , time , winddirection) VALUES ( 1 ,? ,? ,? ,? , ? , ?)");
+            prstatement.setObject(1, locationData.get("name"));
+            prstatement.setObject(2, obj.get("weathercode"));
+            prstatement.setObject(3, obj.get("temperature"));
+            prstatement.setObject(4, obj.get("windspeed"));
+            prstatement.setObject(5, obj.get("time"));
+            prstatement.setObject(6, obj.get("winddirection"));
+            prstatement.execute();
+
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,17 @@ import com.google.gson.internal.LinkedTreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        /*Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("What location?");
         String location = scanner.nextLine();
         ApiCall caller = new ApiCall(location);
         Map<String, String> locationData = caller.getLocationData();
         LinkedTreeMap <String, Object> currentWeather = caller.getCurrentWeather();
         HashMap<String, Object> weatherData = caller.getWeatherData();
+        System.out.println(location);
         System.out.println(currentWeather);
-        System.out.println(weatherData);*/
+        System.out.println(weatherData);
         DBConnection db = new DBConnection();
+        db.update(currentWeather , locationData);
     }
 }
